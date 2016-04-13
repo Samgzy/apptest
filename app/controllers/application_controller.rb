4fe5ae06866c01277e9d5e3cabad09a14af925f8
samy gonzalez <samygz33@gmail.com>
2016-04-13 16:59:34 +0200

    form embedded/ new fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :resource_name, :resource, :devise_mapping
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   def resource_name
     :user
@@ -14,5 +15,11 @@ class ApplicationController < ActionController::Base
 
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  protected
+
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:firstname, :lastname, :is_photograph, :email, :password) }
   end
 end
