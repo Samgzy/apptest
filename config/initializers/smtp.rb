181502e9553b41a03a8f4c83de138647dafa189b
samy gonzalez <samygz33@gmail.com>
2016-04-14 12:13:00 +0200

    changing redirect after fail sign-up

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
-  address: 'smtp.postmarkapp.com',
+  address: 'smtp-relay.gmail.com',
   port: '25',
-  domain: 'heroku.com',
+  domain: 'aspmx.l.google.com',
   user_name: ENV['POSTMARK_API_TOKEN'],
   password: ENV['POSTMARK_API_TOKEN'],
   authentication: :cram_md5,
